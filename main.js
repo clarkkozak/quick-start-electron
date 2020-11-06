4ffa25405ab87eff509fc229b444b09d6b6f3b28
Clark Kozak <clarkkozak@protonmail.com>
2020-11-06 12:16:27 -0700

    Paircast - https://app.paircast.io/c/NiqhO-pE1/1024396

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ function createWindow () {
     const win = new BrowserWindow({
       width: 800,
       height: 600,
-      webPreferences: {
-        nodeIntegration: true
-      }
+    //   webPreferences: {
+    //     nodeIntegration: false
+    //   }
     })
   
     win.loadFile('index.html')
